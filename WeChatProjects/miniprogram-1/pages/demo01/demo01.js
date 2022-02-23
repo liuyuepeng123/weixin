5efaa9ff7be56454f29f20e17080a822c0ee2834
liuyuepeng123 <39367323+liuyuepeng123@users.noreply.github.com>
2022-02-23 15:59:00 +0800

    Add files via upload

---
+++ WeChatProjects/miniprogram-1/pages/demo01/demo01.js
@@ -0,0 +1,85 @@
+// pages/demo01/demo01.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+        message:"hello",
+        mun:1001,
+        isflag:false,
+        person:[
+          {
+            name:"寒冰",
+            age:18,
+            hobb:["射箭","骑马","乒乓球"]
+          },
+          {
+            name:"蛮王",
+            age:26,
+            hobb:["大刀","俯卧撑","跑步"]
+          },
+          {
+            name:"探险家",
+            age:22,
+            hobb:["探险","光枪","闪避"]
+          }
+        ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
